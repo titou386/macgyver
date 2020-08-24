56b664fcb9df3e1747f27f2d9167d2aa27fd16d9
Yves <yves.dornier@yahoo.fr>
2020-08-24 17:52:00 +0200

    added file macgyver.py

---
+++ macgyver.py
@@ -0,0 +1,39 @@
+
+
+
+
+
+class Items:
+	def __init__(self, file_name, coord_x, coord_y)
+		pass
+
+class Personage:
+	def __init__(self, file_name, coord_x, coord_y, evil)
+	  self.coord_x = coord_x
+	  self.coord_y = coord_y
+	  self.evil = evil
+		self.nb_item_collected = 0
+
+	
+	def move(direction):
+		pass
+
+class Zone:
+	def __init__(self, map_file_name)
+		height = 0
+		width  = 0
+		# load map_file_name in memory
+
+# def update_zone():
+#		pass
+
+class Brick
+	def __init__(self, file_name, coord_x, coord_y)
+		pass
+
+
+def console_print():
+	pass
+
+
+#main code
